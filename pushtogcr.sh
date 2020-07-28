351392c77b58881317be4dc8741aaa92bd16cb39
chsteven <chsteven@gmail.com>
2020-07-28 10:35:30 +0800

    Update pushtogcr.sh

--- pushtogcr.sh
+++ pushtogcr.sh
@@ -11,13 +11,3 @@ docker tag $SOURCE_IMAGE $HOSTNAME/$GCP_PROJECT_ID/$IMAGE
 
 docker push $HOSTNAME/$GCP_PROJECT_ID/$IMAGE
 
-## reference sample
-export PROJECT_ID=$(gcloud config list project --format "value(core.project)")
-export IMAGE_REPO_NAME=pytorch_custom_container
-export IMAGE_TAG=$(date +%Y%m%d_%H%M%S)
-export IMAGE_URI=gcr.io/$PROJECT_ID/$IMAGE_REPO_NAME:$IMAGE_TAG
-
-docker build -f Dockerfile -t $IMAGE_URI ./
-
-gcloud auth configure-docker
-docker push $IMAGE_URI
